23c81a25e6a7332ed8f38a1cf6cfe012beee1f2c
alejandro.torres <alejandro.torres@holaluz.com>
2023-10-04 00:17:30 +0200

    refactor: cucumber step definitions

--- atm-simulator/src/test/java/com/codurance/atm/StepDefinitions.java
+++ atm-simulator/src/test/java/com/codurance/atm/StepDefinitions.java
@@ -3,9 +3,8 @@ package com.codurance.atm;
 import com.codurance.atm.account.Account;
 import com.codurance.atm.account.AccountService;
 import com.codurance.atm.infrastructure.CliPrompt;
-import com.codurance.atm.screens.TransactionScreen;
 import com.codurance.atm.screens.WelcomeScreen;
-import com.codurance.atm.screens.WithdrawScreen;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -18,37 +17,39 @@ import static org.mockito.Mockito.verify;
 public class StepDefinitions {
 
     private AccountService accountService;
-    private WelcomeScreen welcomeScreen;
-    private WithdrawScreen withdrawScreen;
     private CliPrompt cliPrompt;
+    private AtmSimulator atmSimulator;
+
+    @Before
+    public void beforeScenario() {
+        accountService = mock(AccountService.class);
+        cliPrompt = mock(CliPrompt.class);
+        atmSimulator = new AtmSimulator(new WelcomeScreen(cliPrompt, accountService));
+    }
 
     @Given("there's an Account with number {int}, PIN {int} and balance {int}")
     public void givenExistingAccountNumberAndPinWithBalance(int accountNumber, int pin, int balance) {
-        Account validAccount = new Account(String.valueOf(accountNumber), balance);
-
-        accountService = mock(AccountService.class);
-        given(accountService.findBy(String.valueOf(accountNumber), String.valueOf(pin))).willReturn(validAccount);
+        Account validAccount = new Account(toString(accountNumber), balance);
+        given(accountService.findBy(toString(accountNumber), toString(pin))).willReturn(validAccount);
     }
 
     @And("I entered {int} as Account number and {int} as PIN")
     public void andEnteringAccountNumberAndPin(int accountNumber, int pin) {
-        cliPrompt = mock(CliPrompt.class);
-        given(cliPrompt.accountNumber()).willReturn(String.valueOf(accountNumber));
-        given(cliPrompt.pin()).willReturn(String.valueOf(pin));
-
-        welcomeScreen = new WelcomeScreen(cliPrompt, accountService);
+        given(cliPrompt.accountNumber()).willReturn(toString(accountNumber));
+        given(cliPrompt.pin()).willReturn(toString(pin));
     }
 
     @When("I withdraw {int}")
     public void whenIWithdraw(int ignoredWithdraw) {
-        TransactionScreen transactionScreen = (TransactionScreen) welcomeScreen.show();
-        withdrawScreen = (WithdrawScreen) transactionScreen.show();
+        atmSimulator.showScreen();
     }
 
     @Then("I should see the message \"Your current balance is {int}\".")
     public void shouldSeeTheMessageWithExpectedBalance(int expectedBalance) {
-        withdrawScreen.show();
-
         verify(cliPrompt).promptGenericMessage(String.format("Your current balance is %d", expectedBalance));
+    }
+
+    private String toString(int accountNumber) {
+        return String.valueOf(accountNumber);
     }
 }
