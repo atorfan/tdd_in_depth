75481bdb0730e39e00c7adad684e58e801ced516
alejandro.torres <alejandro.torres@holaluz.com>
2023-10-04 00:46:49 +0200

    refactor: rename substract to subtract

--- calculator/src/test/java/com/codurance/calculator/CalculatorShould.java
+++ calculator/src/test/java/com/codurance/calculator/CalculatorShould.java
@@ -1,6 +1,9 @@
 package com.codurance.calculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -10,7 +13,7 @@ public class CalculatorShould {
     void substractOneNumberToAnother() {
         Calculator calculator = new Calculator();
 
-        int result = calculator.substract(3, 2);
+        int result = calculator.subtract(3, 2);
 
         assertEquals(result, 1);
     }
