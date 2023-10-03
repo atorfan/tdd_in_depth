7b2da235d7209d48fe0a93d27400e0c0ca11cf83
alejandro.torres <alejandro.torres@holaluz.com>
2023-10-04 00:49:38 +0200

    refactor: re-implement multiply logic

--- calculator/src/main/java/com/codurance/calculator/Calculator.java
+++ calculator/src/main/java/com/codurance/calculator/Calculator.java
@@ -7,12 +7,6 @@ public class Calculator {
     }
 
     public int multiply(int firstNumber, int secondNumber) {
-        if (firstNumber == 2 && secondNumber == 3 || firstNumber == 3 && secondNumber == 2) {
-            return 6;
-        }
-        if (firstNumber == 6 && secondNumber == 9) {
-            return 54;
-        }
-        return 0;
+        return firstNumber * secondNumber;
     }
 }
