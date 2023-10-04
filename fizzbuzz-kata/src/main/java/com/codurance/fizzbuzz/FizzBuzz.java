65509c2ea21c1044f0e0ae562fa2f043b96aed6f
alejandro.torres <alejandro.torres@holaluz.com>
2023-10-04 23:03:18 +0200

    feat: Fizz numbers implementation

--- fizzbuzz-kata/src/main/java/com/codurance/fizzbuzz/FizzBuzz.java
+++ fizzbuzz-kata/src/main/java/com/codurance/fizzbuzz/FizzBuzz.java
@@ -6,6 +6,7 @@ package com.codurance.fizzbuzz;
 public class FizzBuzz {
 
     public String convert(int number) {
+        if (number % 3 == 0) return "Fizz";
         return String.valueOf(number);
     }
 }
--- fizzbuzz-kata/src/test/java/com/codurance/fizzbuzz/FizzBuzzShould.java
+++ fizzbuzz-kata/src/test/java/com/codurance/fizzbuzz/FizzBuzzShould.java
@@ -3,6 +3,7 @@
  */
 package com.codurance.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -14,5 +15,11 @@ class FizzBuzzShould {
     @CsvSource({"1,1", "2,2", "4,4"})
     void convert_number_to_FizzBuzz_string(int input, String expectedOutput) {
         assertEquals(expectedOutput, new FizzBuzz().convert(input));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"3", "6", "9"})
+    void convert_multiples_of_3_to_Fizz(int input) {
+        assertEquals("Fizz", new FizzBuzz().convert(input));
     }
 }
